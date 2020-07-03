821b602efacc32e9c0c29f93a11542f92dc596f0
DishonJP <dishonjp11@gmail.com>
2020-07-03 13:43:03 +0530

    add Created app using webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.scss']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    }
+}
